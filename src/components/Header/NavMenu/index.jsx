f2cec7c912d16dfa7a8cd1934b3d8528bffec691
Matheus <mat.hen.gra@gmail.com>
2023-07-08 14:56:18 -0300

    burger menu toggle

--- src/components/Header/NavMenu/index.jsx
+++ src/components/Header/NavMenu/index.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NavMenu.module.scss';
 import { ReactComponent as HamburgerIcon } from 'assets/images/icon-hamburger.svg';
 
 export default function NavMenu() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu(e) {
+        if (!menuOpen){
+            e.target.style.opacity = '50%';
+            setMenuOpen(true);
+            return;
+        }
+        e.target.style.opacity = '100%';
+        setMenuOpen(false);
+        return;
+    }
+
     return (
         <nav className={styles.NavMenu}>
-            <div className={styles.NavMenu_button}>
+            <div className={styles.NavMenu_button} onClick={e => toggleMenu(e)}>
                 <HamburgerIcon />
             </div>
-            <ul className={styles.NavMenu_list}>
+            <ul className={
+                menuOpen ?
+                styles.NavMenu_list :
+                styles.NavMenu_list___hidden
+            }>
                 <li><a href="/">About</a></li>
                 <li><a href="/">Services</a></li>
                 <li><a href="/">Projects</a></li>
