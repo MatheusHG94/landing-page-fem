0ee4534ef37f13b8e255832ccb745d57f040de14
Matheus <mat.hen.gra@gmail.com>
2023-07-10 12:06:42 -0300

    learn more section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Header from 'components/Header';
+import LearnMoreSection from 'components/LearnMoreSection';
 import 'styles/global.scss';
 
 function App() {
   return (
     <div className='app-container'>
       <Header />
+      <LearnMoreSection 
+        title = 'Transform your brand'
+        summary = 'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.'
+        banner = 'assets/images/mobile/image-transform.jpg'
+        color = 'hsl(51, 100%, 49%)'
+      />
+      <LearnMoreSection 
+        title = 'Stand out to the right audience'
+        summary = 'Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.'
+        banner = 'assets/images/mobile/image-stand-out.jpg'
+        color = 'hsl(7, 99%, 70%)'
+      />
     </div>
   );
 }
---
+++ src/components/LearnMoreSection/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './LearnMoreSection.module.scss';
+
+export default function LearnMoreSection({
+    title,
+    summary,
+    banner,
+    color
+}) {
+  return (
+    <section className={styles.LearnMoreSection}>
+        <div className={styles.LearnMoreSection_banner} style={{
+            background: `${color} url(${banner}) no-repeat center`,
+            backgroundSize: 'cover'
+        }}></div>
+        <div className={styles.LearnMoreSection_content}>
+            <h2>
+                {title}
+            </h2>
+            <p>
+                {summary}
+            </p>
+            <a href='/'>
+                Learn more
+            </a>
+            <div className={styles.LearnMoreSection_underline} style={{
+                backgroundColor: `${color}`
+            }}></div>
+        </div>
+    </section>
+  )
+}
