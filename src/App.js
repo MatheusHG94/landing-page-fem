e79bae79f8f58414a6cf5185986763b044589b9a
Matheus <mat.hen.gra@gmail.com>
2023-07-10 12:47:18 -0300

    highlight section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from 'components/Header';
+import HighlightSection from 'components/HighlightSection';
 import LearnMoreSection from 'components/LearnMoreSection';
 import 'styles/global.scss';
 
@@ -7,16 +8,28 @@ function App() {
     <div className='app-container'>
       <Header />
       <LearnMoreSection 
-        title = 'Transform your brand'
-        summary = 'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.'
-        banner = 'assets/images/mobile/image-transform.jpg'
-        color = 'hsl(51, 100%, 49%)'
+        title='Transform your brand'
+        summary='We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.'
+        banner='assets/images/mobile/image-transform.jpg'
+        color='hsl(51, 100%, 49%)'
       />
       <LearnMoreSection 
-        title = 'Stand out to the right audience'
-        summary = 'Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.'
-        banner = 'assets/images/mobile/image-stand-out.jpg'
-        color = 'hsl(7, 99%, 70%)'
+        title='Stand out to the right audience'
+        summary='Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.'
+        banner='assets/images/mobile/image-stand-out.jpg'
+        color='hsl(7, 99%, 70%)'
+      />
+      <HighlightSection 
+        title='Graphic design'
+        summary='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
+        background='assets/images/mobile/image-graphic-design.jpg'
+        color='hsl(167, 40%, 24%)'
+      />
+      <HighlightSection 
+        title='Photography'
+        summary='Increase your credibility by getting the most stunning, high-quality photos that improve your business image.'
+        background='assets/images/mobile/image-photography.jpg'
+        color='hsl(198, 62%, 26%)'
       />
     </div>
   );
---
+++ src/components/HighlightSection/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './HighlightSection.module.scss';
+
+export default function HighlightSection({
+    title,
+    summary,
+    background,
+    color
+}) {
+  return (
+    <section className={styles.HighlightSection} style={{
+        background: `${color} url(${background}) no-repeat center`,
+        backgroundSize: 'cover',
+        color: `${color}`
+    }}>
+        <h2>
+            {title}
+        </h2>
+        <p>
+            {summary}
+        </p>
+    </section>
+  )
+}
