a9d45c9dbeccd255786be2ea4f60c879c365bff1
Matheus <mat.hen.gra@gmail.com>
2023-07-12 11:22:16 -0300

    gallery layout

--- src/components/TestimonialsSection/Testimonial/index.jsx
+++ src/components/TestimonialsSection/Testimonial/index.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import styles from './Testimonial.module.scss';
 
 export default function Testimonial({
-    key,
     imgSrc,
     testimonial,
     author,
     job
 }) {
   return (
-    <div key={key} className={styles.Testimonial}>
+    <div className={styles.Testimonial}>
         <div className={styles.Testimonial_image}>
             <img src={imgSrc} alt='authors face' />
         </div>
