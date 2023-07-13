f15941802d2fae417ff6b94fbf9c8e9a187e7e99
Matheus <mat.hen.gra@gmail.com>
2023-07-13 10:15:17 -0300

    learn more underline

--- src/components/LearnMoreSection/index.jsx
+++ src/components/LearnMoreSection/index.jsx
@@ -7,31 +7,31 @@ export default function LearnMoreSection({
     banner,
     color
 }) {
-  return (
-    <section className={styles.LearnMoreSection}>
-        <div 
-            className={styles.LearnMoreSection_banner} 
-            style={{
-                background: `${color} url(${banner}) no-repeat center`,
-                backgroundSize: 'cover'
-            }}
-        ></div>
-        <div className={styles.LearnMoreSection_content}>
-            <h2>
-                {title}
-            </h2>
-            <p>
-                {summary}
-            </p>
-            <a href='/'>
-                Learn more
-                <div 
-                    style={{
-                        border: `5px solid ${color}`
-                    }}
-                ></div>
-            </a>
-        </div>
-    </section>
-  )
+    return (
+        <section className={styles.LearnMoreSection}>
+            <div
+                className={styles.LearnMoreSection_banner}
+                style={{
+                    background: `${color} url(${banner}) no-repeat center`,
+                    backgroundSize: 'cover'
+                }}
+            ></div>
+            <div className={styles.LearnMoreSection_content}>
+                <h2>
+                    {title}
+                </h2>
+                <p>
+                    {summary}
+                </p>
+                <a href='/'>
+                    Learn more
+                    <div
+                        style={{
+                            border: `5px solid ${color}`
+                        }}
+                    ></div>
+                </a>
+            </div>
+        </section>
+    )
 }
