c6674a16b8a00002a8e7de0d1d5610c0e768754b
Matheus <mat.hen.gra@gmail.com>
2023-07-15 17:20:35 -0300

    header desktop

--- src/components/Header/NavMenu/index.jsx
+++ src/components/Header/NavMenu/index.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './NavMenu.module.scss';
 import { ReactComponent as HamburgerIcon } from 'assets/images/icon-hamburger.svg';
 
 export default function NavMenu() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    });
 
     const [menuOpen, setMenuOpen] = useState(false);
 
     function toggleMenu(e) {
-        if (!menuOpen){
+        if (!menuOpen) {
             setMenuOpen(true);
             return;
         }
@@ -17,19 +30,22 @@ export default function NavMenu() {
 
     return (
         <nav className={styles.NavMenu}>
-            <div 
+            <div
                 className={`
                     ${styles.NavMenu_button}
                     ${menuOpen ? styles.NavMenu_button___dim : ''}
                 `}
+                style={windowWidth >= 1440 ? { display: 'none' } : {}}
                 onClick={e => toggleMenu(e)}
             >
                 <HamburgerIcon />
             </div>
             <ul className={
-                menuOpen ?
-                styles.NavMenu_list :
-                styles.NavMenu_list___hidden
+                windowWidth >= 1440 ?
+                    styles.NavMenu_list :
+                    menuOpen ?
+                        styles.NavMenu_list :
+                        styles.NavMenu_list___hidden
             }>
                 <li><a href="/">About</a></li>
                 <li><a href="/">Services</a></li>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 import { ReactComponent as Logo } from 'assets/images/logo.svg';
 import { ReactComponent as ArrowIcon } from 'assets/images/icon-arrow-down.svg';
 import NavMenu from './NavMenu';
-import banner from 'assets/images/mobile/image-header.jpg';
+import bannerMobile from 'assets/images/mobile/image-header.jpg';
+import bannerDesktop from 'assets/images/desktop/image-header.jpg';
 
 export default function Header() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+
   return (
     <header
       className={styles.Header}
-      style={{
-        background: `url(${banner}) no-repeat center`,
-        backgroundSize: 'cover'
-      }}
+      style={
+        windowWidth >= 1440 ?
+          { backgroundImage: `url(${bannerDesktop})` } :
+          { backgroundImage: `url(${bannerMobile})` }
+      }
     >
       <div className={styles.Header_bar}>
         <a href="/">
